846f7e71300f321a50d490c8d1c63a65e17e8109
Nicolas Dossou-Gbété <ndossougbe@mail>
2012-08-25 16:13:46 +0200

    changement de la page d'accueil

--- Config/routes.php
+++ Config/routes.php
@@ -25,7 +25,7 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+	Router::connect('/', array('controller' => 'videos', 'action' => 'index'));
 	Router::connect('/video/:id', array('controller' => 'videos', 'action' => 'show'), array('pass' => array('id'), 'id' => '[0-9]+'));
 /**
  * ...and connect the rest of 'Pages' controller's urls.
